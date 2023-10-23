4ebfcf725b2f32c4dc38b9727a7516bbf1571eb9
Simen Dymbe <simen.dymbe@gmail.com>
2023-10-24 00:48:48 +0200

    Add device as param

--- training.py
+++ training.py
@@ -6,11 +6,12 @@ from torch.nn.modules.loss import _Loss
 from torch.optim import Optimizer
 
 
-def train_epoch(model: nn.Module, dataloader: DataLoader, loss: _Loss, optimizer: Optimizer):
+def train_epoch(model: nn.Module, dataloader: DataLoader, loss: _Loss, optimizer: Optimizer, device: torch.device):
     loss_sum = 0
     batches = 0
 
     for image_batch in dataloader:
+        image_batch = image_batch.to(device)
     
         out, _ = model(image_batch)
         loss = loss(out, image_batch)
@@ -26,12 +27,14 @@ def train_epoch(model: nn.Module, dataloader: DataLoader, loss: _Loss, optimizer
     return loss_sum / batches
 
 
-def validate(model: nn.Module, dataloader: DataLoader, loss: _Loss, optimizer: Optimizer):
+def validate(model: nn.Module, dataloader: DataLoader, loss: _Loss, optimizer: Optimizer, device: torch.device):
     loss_sum = 0
     batches = 0
 
     with torch.no_grad():
         for image_batch in dataloader:
+            image_batch = image_batch.to(device)
+
             out, _ = model(image_batch)
             loss = loss(out, image_batch)
 
